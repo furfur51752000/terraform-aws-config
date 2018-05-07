7c2afdd2b60a2844d2a4080528dbe62c2c883f1e
jake.yang <jake.yang@jakeyang.local>
2018-05-07 14:46:36 +0800

    adjust the source

--- module.tf
+++ module.tf
@@ -1,6 +1,5 @@
 module "config" {
-  source  = "trussworks/config/aws"
-  version = "1.0.1"
+  source             = "github.com/furfur51752000/terraform-aws-config"
   config_logs_bucket = "104isgd-config-test"
 
   # insert the 1 required variable here
