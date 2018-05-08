21b7e4c15eeb375f5a23a43d2a00e03c80e78363
jake.yang <jake.yang@jakeyang.local>
2018-05-08 15:29:47 +0800

    delete module.tf

--- module.tf
+++
@@ -1,6 +0,0 @@
-module "config" {
-  source             = "github.com/furfur51752000/terraform-aws-config"
-  config_logs_bucket = "104isgd-config-test"
-
-  # insert the 1 required variable here
-}
